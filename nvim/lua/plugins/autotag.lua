7ad8b0f4aec556e8d2bf95c37b0748cd70ddbeba
Thomas <thomascoxpowell@gmail.com>
2025-05-17 17:29:49 -0400

    snacks

--- nvim/lua/plugins/autotag.lua
+++ nvim/lua/plugins/autotag.lua
@@ -5,6 +5,6 @@ return {
     enable_close = true, -- Auto close tags
     enable_rename = true, -- Auto rename pairs of tags
     enable_close_on_slash = false -- Auto close on trailing </
-  },
+  }
 }
 
