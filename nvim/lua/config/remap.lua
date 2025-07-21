6b5994eb0b591918f95fd24cbf518ac9b4b3841f
Thomas <43977109+thomascpowell@users.noreply.github.com>
2025-07-21 14:14:07 -0400

    cleaning up my terrible nvim config

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -1,10 +1,10 @@
 vim.g.mapleader = " "
 
 -- example remap: while in normal mode, leaderpv executes :Ex
--- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 
 -- toggle nvim-tree
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
-
+-- cycle through buffers
 vim.keymap.set("n", "<leader><tab>", ":b<space><Tab>")
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "neovim/nvim-lspconfig"
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig"
   },
   config = function()
     require("mason").setup()
