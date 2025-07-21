921f8191ebc2f9c98921f646524860aa0e4374b1
Thomas <43977109+thomascpowell@users.noreply.github.com>
2025-07-21 13:51:53 -0400

    Revert "?"
    This reverts commit 5c9b70310af79d007886c91071dd9b1dafaf72e0.

--- _nvim/lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8',
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>,', builtin.buffers, {})
-    end,
-}
