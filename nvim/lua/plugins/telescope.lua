e7ac4c655cdb37a3cc7a49e03109da267df7b091
Thomas <43977109+thomascpowell@users.noreply.github.com>
2025-08-09 18:51:40 -0400

    new binds for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,10 @@
 return {
-  'nvim-telescope/telescope.nvim', 
+  'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>,', builtin.buffers, {})
+    vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = 'Telescope buffers' })
   end,
 }
