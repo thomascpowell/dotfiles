05b6b291b7c9740335811a5627d4babed0965ec9
Thomas <thomascoxpowell@gmail.com>
2025-09-06 16:16:23 -0400

    add rounded borders to hover

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -11,9 +11,17 @@ vim.keymap.set("n", "<leader><tab>", ":b<space><Tab>")
 
 -- lsp binds
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
--- vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+
 vim.keymap.set("n", "<leader>lf", function()
   require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format with Conform" })
+
 vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+
 vim.keymap.set("n", "<leader>k", function() vim.diagnostic.open_float() end, opts)
+
+vim.keymap.set('n', 'K', function()
+  vim.lsp.buf.hover({
+    border = "rounded"
+  })
+end, { noremap = true, silent = true })
