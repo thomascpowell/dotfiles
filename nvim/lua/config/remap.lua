260292afba25d3694316c369a9ea1d7ea118c8db
Thomas <thomascoxpowell@gmail.com>
2025-09-11 17:55:27 -0400

    zz does not work well with smooth scroll

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -5,11 +5,6 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
 
 
--- up/down auto centers
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("x", "<C-u>", "<C-u>zz")
-vim.keymap.set("x", "<C-d>", "<C-d>zz")
 -- cycle through buffers
 vim.keymap.set("n", "<leader><tab>", ":b<space><Tab>")
 
