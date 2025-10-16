bd58c3859df595015f7fd752adffeb64a65bdeae
Thomas <43977109+thomascpowell@users.noreply.github.com>
2025-10-16 15:47:02 -0400

    font size and conform changes

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,6 +5,11 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         python = { "ruff" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+
       },
       default_format_opts = {
         lsp_format = "fallback",
