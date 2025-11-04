3a8c45563566b02aad7d9e50df0b6fa7492185c7
Thomas <thomascoxpowell@gmail.com>
2025-11-04 11:23:58 -0500

    .

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,29 +1,12 @@
--- pyright
 require 'lspconfig'.pyright.setup {}
 require 'lspconfig'.autopep8.setup {}
-
--- lua
 require 'lspconfig'.lua_ls.setup {}
-
--- lua
 require 'lspconfig'.clangd.setup {}
-
 vim.lsp.enable("millet")
-
--- tsserver
 require 'lspconfig'.ts_ls.setup {}
-
--- cssls
 require 'lspconfig'.cssls.setup {}
-
--- svelte
 vim.lsp.enable("svelte")
-
 vim.lsp.enable("sqlls")
-
--- json
 require 'lspconfig'.jsonls.setup {}
-
--- docker
 require 'lspconfig'.dockerls.setup {}
 require 'lspconfig'.docker_compose_language_service.setup {}
