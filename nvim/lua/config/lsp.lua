ca332db78f0b07f4472a694ae15674eb45fcd01a
Thomas <thomascoxpowell@gmail.com>
2025-11-07 14:01:30 -0500

    feat: add php support (unfortunately)

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,5 +8,6 @@ require 'lspconfig'.cssls.setup {}
 vim.lsp.enable("svelte")
 vim.lsp.enable("sqlls")
 require 'lspconfig'.jsonls.setup {}
+require 'lspconfig'.phpactor.setup {}
 require 'lspconfig'.dockerls.setup {}
 require 'lspconfig'.docker_compose_language_service.setup {}
