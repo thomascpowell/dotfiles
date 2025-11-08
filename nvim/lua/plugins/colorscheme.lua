a2aef392ba7807c6e3dc20b3c3d066acb6060486
Thomas <thomascoxpowell@gmail.com>
2025-11-08 13:10:01 -0500

    update nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,20 @@
+-- fdsaf
+
 return {
   "slugbyte/lackluster.nvim",
   lazy = false,
   priority = 1000,
-  init = function()
-    require("lackluster").setup({
+  config = function()
+    local lackluster = require("lackluster")
+    local color = lackluster.color
+
+    lackluster.setup({
       tweak_background = {
         normal = 'none',
         telescope = 'none',
+      },
+      tweak_syntax = {
+        comment = color.gray6
       },
     })
     vim.cmd.colorscheme("lackluster")
