917b2963856403418f15073d0060f4025f09a6c7
Thomas <43977109+thomascpowell@users.noreply.github.com>
2025-11-13 13:08:22 -0500

    refactor: fully migrate to new vim lsp system

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,13 +1,28 @@
-require 'lspconfig'.pyright.setup {}
-require 'lspconfig'.autopep8.setup {}
-require 'lspconfig'.lua_ls.setup {}
-require 'lspconfig'.clangd.setup {}
+-- new
 vim.lsp.enable("millet")
-require 'lspconfig'.ts_ls.setup {}
-require 'lspconfig'.cssls.setup {}
 vim.lsp.enable("svelte")
 vim.lsp.enable("sqlls")
-require 'lspconfig'.jsonls.setup {}
-require 'lspconfig'.phpactor.setup {}
-require 'lspconfig'.dockerls.setup {}
-require 'lspconfig'.docker_compose_language_service.setup {}
+
+-- updated
+vim.lsp.enable("pyright")
+vim.lsp.enable("autopep8")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("cssls")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("phpactor")
+vim.lsp.enable("dockerls")
+vim.lsp.enable("docker_compose_language_service")
+
+-- deprecated
+-- require 'lspconfig'.pyright.setup {}
+-- require 'lspconfig'.autopep8.setup {}
+-- require 'lspconfig'.lua_ls.setup {}
+-- require 'lspconfig'.clangd.setup {}
+-- require 'lspconfig'.ts_ls.setup {}
+-- require 'lspconfig'.cssls.setup {}
+-- require 'lspconfig'.jsonls.setup {}
+-- require 'lspconfig'.phpactor.setup {}
+-- require 'lspconfig'.dockerls.setup {}
+-- require 'lspconfig'.docker_compose_language_service.setup {}
