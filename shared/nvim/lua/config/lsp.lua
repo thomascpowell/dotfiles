75a8ef50816c5380308875d82b1fea21771e7137
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-01-01 16:06:20 -0500

    feat: remove mason finally

--- shared/nvim/lua/config/lsp.lua
+++ shared/nvim/lua/config/lsp.lua
@@ -1,29 +1,14 @@
--- new
-vim.lsp.enable("millet")
 vim.lsp.enable("svelte")
-vim.lsp.enable("sqlls")
 vim.lsp.enable("bashls")
-
--- updated
 vim.lsp.enable("pyright")
-vim.lsp.enable("autopep8")
 vim.lsp.enable("lua_ls")
-vim.lsp.enable("clangd")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("cssls")
-vim.lsp.enable("jsonls")
-vim.lsp.enable("phpactor")
-vim.lsp.enable("dockerls")
-vim.lsp.enable("docker_compose_language_service")
+vim.lsp.enable("rust_analyzer")
 
--- deprecated
--- require 'lspconfig'.pyright.setup {}
--- require 'lspconfig'.autopep8.setup {}
--- require 'lspconfig'.lua_ls.setup {}
--- require 'lspconfig'.clangd.setup {}
--- require 'lspconfig'.ts_ls.setup {}
--- require 'lspconfig'.cssls.setup {}
--- require 'lspconfig'.jsonls.setup {}
--- require 'lspconfig'.phpactor.setup {}
--- require 'lspconfig'.dockerls.setup {}
--- require 'lspconfig'.docker_compose_language_service.setup {}
+-- vim.lsp.enable("dockerls")
+-- vim.lsp.enable("clangd")
+-- vim.lsp.enable("jsonls")
+-- vim.lsp.enable("sqlls")
+-- vim.lsp.enable("docker_compose_language_service")
+-- vim.lsp.enable("phpactor")
--- shared/nvim/lua/plugins/lsp.lua
+++ shared/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,3 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig"
-  },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup()
-  end,
+  "neovim/nvim-lspconfig"
 }
