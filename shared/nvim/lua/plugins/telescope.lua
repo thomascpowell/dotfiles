2bccde3c91ade21f7ef2c3af30d17a7e48a7b7de
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-01-04 18:33:24 -0500

    feat: use git_files in telescope

--- shared/nvim/lua/plugins/telescope.lua
+++ shared/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
     tag = '0.1.8',
     config = function()
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader><leader>', builtin.git_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>ff', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>,', builtin.buffers, { desc = 'Telescope buffers' })
     end,
