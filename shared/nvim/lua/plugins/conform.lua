cc4ddcaa304896131922afb2df6582bb66ca3f19
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-01-10 12:54:36 -0500

    feat: make yazi look more like lf

--- shared/nvim/lua/plugins/conform.lua
+++ shared/nvim/lua/plugins/conform.lua
@@ -1,20 +1,20 @@
 -- formatters
 
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-                python = { "ruff" },
-                typescript = { "prettier" },
-                javascript = { "prettier" },
-                typescriptreact = { "prettier" },
-                json = { "prettier" },
-            },
-            default_format_opts = {
-                lsp_format = "fallback",
-            },
-        })
-    end,
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        python = { "ruff" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+        typescriptreact = { "prettier" },
+        json = { "prettier" },
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+    })
+  end,
 }
---
+++ shared/yazi/plugins/no-status.yazi/main.lua
@@ -0,0 +1,17 @@
+--- Plugin: "no-status.yazi"
+--- URL: https://github.com/yazi-rs/plugins/tree/main/no-status.yazi
+--- License: MIT
+
+--- @since 25.2.7
+
+local function setup()
+	local old_layout = Tab.layout
+
+	Status.redraw = function() return {} end
+	Tab.layout = function(self, ...)
+		self._area = ui.Rect { x = self._area.x, y = self._area.y, w = self._area.w, h = self._area.h + 1 }
+		return old_layout(self, ...)
+	end
+end
+
+return { setup = setup }
