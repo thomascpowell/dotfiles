0a6e090cb1ddc9ce7932ddc00a743f303b367a36
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-01-18 12:30:32 -0500

    fix: python conform

--- shared/nvim/lua/plugins/conform.lua
+++ shared/nvim/lua/plugins/conform.lua
@@ -5,11 +5,19 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("conform").setup({
+      formatters = {
+        ruff = {
+          command = "ruff",
+          args = { "format", "-" },
+          stdin = true,
+        },
+      },
       formatters_by_ft = {
         typescript = { "prettier" },
         javascript = { "prettier" },
         typescriptreact = { "prettier" },
         json = { "prettier" },
+        python = { "ruff" },
       },
       default_format_opts = {
         lsp_format = "fallback",
