8b897b97172825d56d2bd796bc866de832f387ac
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-03-02 14:54:39 -0500

    fix: treesitter ensure_installed

--- shared/nvim/lua/plugins/treesitter.lua
+++ shared/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,15 @@
+local langs = { "typescript", "javascript", "html", "css", "svelte", "rust", "lua", "python", "c", "markdown", "bash" }
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = langs,
+	callback = function() vim.treesitter.start() end,
+})
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   lazy = false,
-  tag = "v0.10.0",
   opts = {
+    ensure_installed = langs,
     sync_install = false,
     highlight = { enable = true },
     indent = { enable = true },
