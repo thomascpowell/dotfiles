6a7729b96c37f232f1f3e4f08d0d840fdaff1fbf
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-04-10 01:04:58 -0400

    feat: theme

---
+++ programs/nvim/lua/config/md.lua
@@ -0,0 +1,15 @@
+-- remaps that apply in markdown and typst files, for writing purposes
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "typst" },
+  callback = function()
+    local opt = vim.opt_local
+    opt.wrap = true
+    opt.linebreak = true
+    opt.breakindent = true
+    vim.keymap.set("n", "j", "gj", { buffer = true })
+    vim.keymap.set("n", "k", "gk", { buffer = true })
+    vim.keymap.set("v", "j", "gj", { buffer = true })
+    vim.keymap.set("v", "k", "gk", { buffer = true })
+  end,
+})
