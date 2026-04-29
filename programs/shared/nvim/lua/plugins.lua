03ffbcca122d227040105b08bfac0fd7e704faaf
Thomas <43977109+thomascpowell@users.noreply.github.com>
2026-04-29 13:32:25 -0400

    fix: blink.lib

--- programs/shared/nvim/lua/plugins.lua
+++ programs/shared/nvim/lua/plugins.lua
@@ -10,6 +10,7 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/saghen/blink.lib" },
   { src = "https://github.com/L3MON4D3/LuaSnip" },
   { src = "https://github.com/rafamadriz/friendly-snippets" }
 })
